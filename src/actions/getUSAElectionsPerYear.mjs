77d59e950d8e61cf4da0168b1d384c8143427d31
MrMaia <mrmaiacodes@gmail.com>
2024-04-22 12:11:24 -0300

    added startChrome.bat description to README

--- src/actions/getUSAElectionsPerYear.mjs
+++ src/actions/getUSAElectionsPerYear.mjs
@@ -3,4 +3,5 @@ import preparePage from "../preparePage.mjs";
 export default async function getUSAElectionsPerYear(browser) {
     const page = await preparePage(browser);
     await page.goto("https://www.opensecrets.org/presidential-elections");
+    await page.waitForSelector('h2>a[href*="pres"]');
 }
