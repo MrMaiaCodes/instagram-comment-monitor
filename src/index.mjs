9c74d3e759f4097c83306dee3fbe95c5c73708c7
MrMaia <mrmaiacodes@gmail.com>
2024-07-14 15:35:37 -0300

    pulled up all posts from page, doing universal prompt next

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,25 @@
 import "dotenv/config"
 import prepareBrowser from "./prepareBrowser.mjs";
 import preparePage from "./preparePage.mjs"
-import { accessInstagramPost } from "./actions/accessInstagramPost.mjs";
-import getUSAElectionsPerYear from "./actions/getUSAElectionsPerYear.mjs";
-import getUSAElections from "./actions/getUSAElections.mjs";
+import { accessProfilePage } from "./actions/accessProfilePage.mjs";
+import { getPosts } from "./actions/getPosts.mjs";
+import { accessPostPage } from "./actions/accessPostPage.mjs";
+
 //this runs the system
 async function main(){
     console.log("Hello world!");
     const browser = await prepareBrowser();
-    const elections = await getUSAElectionsPerYear(browser);
-    await getUSAElections(browser, elections);
-    //await loginInstagram(page, process.env.LOGIN, process.env.PASSWORD);
-   // await accessInstagramPost(page, process.env.LOGIN)
+    const page = await preparePage(browser);
+    const profilePage = await accessProfilePage(page, "pulseofpal");
+    const posts = await getPosts(profilePage);
+    console.log(posts);
+    const postPages = [];
+    for await (const post of posts.slice(0,1)){
+      const page = await preparePage(browser);
+      const pageReady = await accessPostPage(page, post);
+      postPages.push(pageReady);
+    }
+
 }
 
 
